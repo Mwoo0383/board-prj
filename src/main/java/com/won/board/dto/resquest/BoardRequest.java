cd8ce6b0d62ab244452d6664a5a55aec6cd5ba78
Minwoo <cap019@naver.com>
2025-03-10 18:51:21 +0900

    refactor: BoardRequest Dto 추가

---
+++ src/main/java/com/won/board/dto/resquest/BoardRequest.java
@@ -0,0 +1,30 @@
+package com.won.board.dto.resquest;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BoardRequest {
+    private String title;
+    private String content;
+
+    public BoardRequest(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
